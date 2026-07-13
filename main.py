add74f9bca360c827b5c95e8b7e043ae228bd22d
Aleksander Vizvary <a.vizvary@icloud.com>
2026-07-13 23:29:07 +0200

    Upgraded readibility

--- main.py
+++
@@ -1,8 +0,0 @@
-from Functions.final_combining import start
-
-event_name = "Praca"
-adress = "ul. Pawia 5, 31-154, Kraków, Polska"
-test_namne = "Aleksander Vizvary"
-start(event_name, adress, test=True, test_name=test_namne)
-
-
